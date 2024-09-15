733b8df3ed9b19ff0649088d9effc19d2ce36ece
Jack Jordan <jjordan6591@gmail.com>
2024-09-15 11:54:31 -0400

    Initial Commit

---
+++ TrackerApp/TrackerAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  TrackerAppApp.swift
+//  TrackerApp
+//
+//  Created by Jack Jordan on 9/15/24.
+//
+
+import SwiftUI
+
+@main
+struct TrackerAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
